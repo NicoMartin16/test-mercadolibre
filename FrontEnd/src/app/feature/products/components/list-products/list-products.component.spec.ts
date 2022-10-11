faddbfc0106fe9ef455f0c96cd4d94094dc3d845
NicoMartin16 <nicolasfelipemartin@hotmail.com>
2022-10-11 14:39:15 -0500

    se agregan unit test en listado

--- FrontEnd/src/app/feature/products/components/list-products/list-products.component.spec.ts
+++ FrontEnd/src/app/feature/products/components/list-products/list-products.component.spec.ts
@@ -1,34 +1,50 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { ListProductsComponent } from './list-products.component';
-import { RouterTestingModule } from '@angular/router/testing';
-import { ItemService } from '../../shared/services/item.service';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ListProductsComponent } from "./list-products.component";
+import { RouterTestingModule } from "@angular/router/testing";
+import { ItemService } from "../../shared/services/item.service";
 
-describe('ListProductsComponent', () => {
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { Item } from "src/app/core/models/item.model";
+
+import { listItems } from "../../../../shared/mocks/items.mock";
+import { Router } from "@angular/router";
+
+const routerSpy = { navigate: jasmine.createSpy("navigate") };
+
+describe("ListProductsComponent", () => {
   let component: ListProductsComponent;
   let fixture: ComponentFixture<ListProductsComponent>;
 
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ListProductsComponent ],
-      imports: [
-        RouterTestingModule,
-        HttpClientTestingModule
-      ],
+      declarations: [ListProductsComponent],
+      imports: [RouterTestingModule, HttpClientTestingModule],
       providers: [
-        ItemService
-      ]
-    })
-    .compileComponents();
+        ItemService,
+        {
+          provide: Router,
+          useValue: routerSpy,
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ListProductsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("debe redireccionar al detalle del item", () => {
+    // arrange
+    let mockItem: Item = listItems[0];
+    // act
+    component.navigateDetail(mockItem);
+    // assert
+    expect(routerSpy.navigate).toHaveBeenCalledWith([`item/${mockItem.id}`]);
   });
 });
